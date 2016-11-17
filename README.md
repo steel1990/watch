1ff805a06eb07a020d64447559c79da5014c9124
steel1990 <lbbsteel@gmail.com>
2016-11-17 16:58:12 +0800

    1. add ${p} and ${n} for the commond replacer 2. update the readme doc

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-A file watch command line tool for run shell while file changed.
+cli-watch is a cmd tool to run shell while files changed.
+
+## Install
+
+`npm i -g cli-watch`
+
+## Usage
+
+`watch -f a.js -r 'uglifyjs a.js -o b.js'`
+
+This command will watch `a.js`, and run `uglifyjs a.js -o b.js` when it changed. 
+
+You can use glob patterns for the files param, such as:
+
+`watch -f *.js -r 'uglifyjs ${p} -o ${n}.min.js'`
+
+
+### Options
+
+    -h, --help               output usage information
+    -V, --version            output the version number
+    -f, --files [value]      glob files pattern, use ; to separate multiple path
+    -r, --run [value]        run command while files changed, you can use ${p} to instead of the changed file path, and use ${n} to instead of the name of changed file path without ext
+    -n, --dontRunWhileStart  dont run the command while start watch
--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 
 const gaze = require('gaze');
 const program = require('commander');
-const debug = require('debug')('index');
+const shell = require('shelljs');
+const path = require('path');
+const debug = require('debug')('cli-watch');
 const pkg = require('./package.json');
-const exec = require('child_process').exec;
 
 program
     .version(pkg.version)
-    .option('-f, --files [value]', 'glob files pattern，多个使用;分隔')
-    .option('-r, --run [value]', 'run command white file changed')
+    .option('-f, --files [value]', 'glob files pattern, use ; to separate multiple path')
+    .option('-r, --run [value]', 'run command while files changed, you can use ${p} to instead of the changed file path, and use ${n} to instead of the name without extension')
+    .option('-n, --dontRunWhileStart', 'dont run the command while start watch')
     .parse(process.argv);
 
 if (!program.files || !program.run) {
@@ -20,19 +22,31 @@ if (!program.files || !program.run) {
 debug('files', program.files);
 debug('run', program.run);
 
+function run(changedPath) {
+    debug('run', changedPath);
+    var cmd = program.run;
+    if (changedPath) {
+        cmd = cmd.replace(/\$\{p\}/g, `"${changedPath}"`);
+        var name = path.basename(changedPath, path.extname(changedPath));
+        cmd = cmd.replace(/\$\{n\}/g, `${name}`);
+    } else if (/\$\{p\}/.test(cmd)) {
+        debug('run', 'Dont run the cmd because of it need changed file path');
+        return;
+    }
+    debug('run', cmd);
+    shell.exec(cmd);
+    debug('run', 'end');
+    console.log('shell run end');
+}
+
+if (!program.dontRunWhileStart) {
+    run();
+}
+
 gaze(program.files.split(';'), (err, watcher) => {
     debug('start success', watcher.relative());
     watcher.on('all', (evt, filepath) => {
         debug('file changed', filepath);
-        exec(program.run, (err, stdout, stderr) => {
-            if (err) {
-                debug('cmd run error');
-                console.error(`exec error: ${err}`);
-                return;
-            }
-            debug('cmd run done');
-            process.stdout.write(stdout);
-            process.stderr.write(stderr);
-        });
+        run(filepath);
     });
 });
